b5b0fc92e6a5d0c1fdd6d651aca17708dcc31895
AnwarAsry <anwareducation03@gmail.com>
2026-02-21 23:47:17 +0100

    repos

---
+++ src/main/java/org/iths/ecommercebe/Repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package org.iths.ecommercebe.Repository;
+
+import org.iths.ecommercebe.Model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
