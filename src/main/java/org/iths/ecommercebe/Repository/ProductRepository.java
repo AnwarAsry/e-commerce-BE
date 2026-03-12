a140bdffba48f8105542975b8a230cab1725e837
AnwarAsry <anwareducation03@gmail.com>
2026-03-12 09:36:56 +0100

    updated product service

--- src/main/java/org/iths/ecommercebe/Repository/ProductRepository.java
+++ src/main/java/org/iths/ecommercebe/Repository/ProductRepository.java
@@ -1,7 +1,27 @@
 package org.iths.ecommercebe.Repository;
 
+import org.iths.ecommercebe.Model.Category;
 import org.iths.ecommercebe.Model.Product;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategoryAndActiveTrue(Category category, Sort sort);
+
+    List<Product> findByFeaturedTrueAndActiveTrue();
+
+    @Query("""
+                SELECT p FROM Product p
+                WHERE p.active = true
+                AND (
+                    LOWER(p.name) LIKE LOWER(CONCAT('%', :query, '%'))
+                )
+            """)
+    List<Product> searchByName(@Param("query") String query);
 }
